3246c7e16ec237859f583fbac634ea2ac9d8667e
Malone <malone@gmail.com>
2025-03-29 01:01:08 +0800

    基于lua脚本，判断秒杀库存、一人一单、扣减库存等

---
+++ src/main/resources/seckill.lua
@@ -0,0 +1,17 @@
+local userId = ARGV[1]
+local voucherId = ARGV[2]
+
+local stockKey = 'seckill:stock:' .. voucherId
+local orderKey = 'seckill:order:' .. userId
+
+if (tonumber(redis.call('get', stockKey)) <= 0) then
+    return 1
+end
+
+if (redis.call('sismember', orderKey, userId) == 1) then
+    return 2
+end
+
+redis.call('incrby', stockKey, -1)
+redis.call('sadd', orderKey, userId)
+return 0
